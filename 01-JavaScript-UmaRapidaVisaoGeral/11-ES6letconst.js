b434e591f347e0e7e0703bce1f659e6f569a6a17
José Stélio R. Malcher Junior <contato@josemalcher.net>
2017-12-06 15:40:58 -0300

    11 - ES6 let e const

---
+++ 01-JavaScript-UmaRapidaVisaoGeral/11-ES6letconst.js
@@ -0,0 +1,46 @@
+//ES 6
+
+// EcmaScript 6 Constants
+const PI = 3.141593;
+//PI = 3.0; //throws error
+console.log(PI);
+
+//******* EcmaScript 6: let is the new var
+var frame = 'Angular';
+var frame = 'React';
+console.log(frame);
+
+let varES6 = 'javaScript';
+//let varES6 = 'JAVA'; //dá erro
+
+let movie = 'Lord of the Rings';
+//var movie = 'Batman v Superman'; //throws error, variable movie already declared
+
+function starWarsFan() {
+    let movie = 'Fime Star Wars 01';
+    return movie;
+}
+
+function marvelFan() {
+    movie = 'The averangers';
+    return movie;
+}
+
+function blizzardFan() {
+    let isFan = true;
+    let phrase = 'Wordcraft';
+    console.log('Before if: ' + phrase);
+    if(isFan){
+        let phrase = 'Texto inicial';
+        phrase = 'For the Horde!!';
+        console.log('O que sobrou... '+ phrase);
+    }
+    phrase = 'Para Aliança....';
+    console.log('Depois do se... '+ phrase);
+}
+
+console.log('---'+movie+'---');
+console.log(starWarsFan());
+console.log(marvelFan());
+console.log('---'+movie+'---');
+blizzardFan();
