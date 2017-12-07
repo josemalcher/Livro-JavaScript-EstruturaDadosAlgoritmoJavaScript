7c79c4c48b7178a1006566b921f09db2a0811bbe
José Stélio R. Malcher Junior <contato@josemalcher.net>
2017-12-06 22:19:57 -0300

    14 - ES6 Parameter Handling

--- 01-JavaScript-UmaRapidaVisaoGeral/js/main.js
+++ 01-JavaScript-UmaRapidaVisaoGeral/js/main.js
@@ -1,58 +1,41 @@
-'use strict';
+"use strict";
 
-var circulo = function circulo(r) {
-    var PI = 3.14;
-    var area = PI * r * r;
-    return area;
+//******* EcmaScript 6: Default Parameter Values
+function soma() {
+    var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
+    var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 2;
+    var z = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 3;
+
+    return x + y + z;
 };
-console.log(circulo(2));
+console.log(soma(10, 20));
 
-//ou
-
-var cirtuclo2 = function cirtuclo2(r) {
-    return 3.14 * r * r;
+//function above is the same as
+function sum2(x, y, z) {
+    if (x === undefined) x = 1;
+    if (y === undefined) y = 2;
+    if (z === undefined) z = 3;
+    return x + y + z;
 };
-console.log(cirtuclo2(4));
+console.log(sum2(4, 2)); //outpus 9
 
-//ou
+//******* EcmaScript 6: spread operator ('...')
+var params = [3, 4, 5];
+console.log(soma.apply(undefined, params)); // 12
 
-var mostraSegundos = function mostraSegundos() {
-    return new Date.getSeconds();
+var numbers = [1, 2].concat(params); //pushing values into array
+console.log(numbers); // (5)[1,2,3,4,5]
+
+
+//******* EcmaScript 6: rest parameter ('...')
+function restParamaterFunction(x, y) {
+    return (x + y) * (arguments.length <= 2 ? 0 : arguments.length - 2);
 };
-var mostraSegundos = function mostraSegundos() {
-    return new Date.getSeconds();
+console.log(restParamaterFunction(1, 2, "hello", true, 7)); // outputs 9;
+
+//code above is the same as ES5:
+function restParamaterFunction2(x, y) {
+    var a = Array.prototype.slice.call(arguments, 2);
+    return (x + y) * a.length;
 };
-var mostraSegundos = function mostraSegundos() {
-    return new Date.getSeconds();
-};
-
-//outro ex;
-
-var objetoUsuaro = function objetoUsuaro(id, nome) {
-    return {
-        id: id,
-        nome: nome
-    };
-};
-var objetoUsuario = function objetoUsuario(id, nome) {
-    return { id: id, nome: nome };
-};
-var objetoUsuario = function objetoUsuario(id, nome) {
-    return { id: id, nome: nome };
-};
-
-//outro ex:
-
-var itemsCarrinho = [{ id: 1, nome: 'Item 01', preco: 1200 }, { id: 2, nome: 'Item 02', preco: 3200 }];
-
-var total = itemsCarrinho.reduce(function (total, item) {
-    return total + item.preco;
-}, 0);
-var total = itemsCarrinho.reduce(function (total, item) {
-    return total + item.preco;
-}, 0);
-
-//resuzido possivel
-var total = itemsCarrinho.reduce(function (total, item) {
-    return total + item.preco;
-}, 0);
+console.log(restParamaterFunction2(1, 2, "hello", true, 7)); // outputs 9;
